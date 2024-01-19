9865e889f899e5e9bb112aa5dc0486d559f55843
priyanka-deshmukh8 <priyankadeshmukh.h@gmail.com>
2024-01-20 04:27:02 +0530

    final

---
+++ src/main/java/com/greenstitch/controller/controller.java
@@ -0,0 +1,70 @@
+package com.greenstitch.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.greenstitch.entity.signup;
+import com.greenstitch.seervices.services;
+
+
+
+@Controller
+public class controller {
+	
+	@Autowired
+	services service;
+	
+	@GetMapping("/home")
+	public String home() {
+		return "index";
+	}
+	
+	@GetMapping("/signin")
+	public String sign() {
+		return "signin";
+	}
+	
+	@GetMapping("/signup")
+	public String signu() {
+		return "signup";
+	}
+	
+	@PostMapping("/addUser")
+	public String addUser(@RequestParam("id")int id, 
+			@RequestParam("name")String name, 
+			@RequestParam("email")String email, 
+			@RequestParam("password")String password) 
+	{
+		boolean idExist = service.id(id);
+		boolean emailExist=service.checkEmail(email);
+		if(!idExist && !emailExist) {
+			signup user = new signup();
+			user.setId(id);
+			user.setName(name);
+			user.setEmail(email);
+			user.setPassword(password);
+			service.addUsers(user);
+
+			return "signin";
+		}
+		else {
+			return "signup";
+		}
+	}
+	@PostMapping("/signin")
+	public String validate(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
+	    boolean flag = this.service.validate(email, password);
+	    if (flag) {
+	        return "successful";
+	    } else {
+	       
+	        return "signin";
+	    }
+	}
+
+}
+	
